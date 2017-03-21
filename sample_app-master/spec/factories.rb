72749e423720b375ba4c47aa6db25f3206a3d455
DrChido <chidozie.mgbemena@hotmail.com>
2017-03-21 22:03:48 +0000

    New test message

---
+++ sample_app-master/spec/factories.rb
@@ -0,0 +1,26 @@
+FactoryGirl.define do 
+
+  factory :relationship do
+    follower_id 1
+    followed_id 1
+  end
+ 
+
+  factory :user do
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
+  end
+
+  factory :micropost do
+    content "Lorem ipsum"
+    user
+  end
+
+end
+
